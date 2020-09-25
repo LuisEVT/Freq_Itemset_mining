42772608ed667a16335ad3305d5613bb82594b3f
Luis Vargas Tamayo <fatherluis98@gmail.com>
2020-09-25 13:17:15 -0500

    Comments:
    -Added comments to guide me through the process
    - Deleted wDATASET since i can just use DATASET... this is my copy of the database to manipulate.

--- freq_mining.py
+++ freq_mining.py
@@ -11,6 +11,12 @@ from scipy.special import comb as nCr
 
 def read_file(fPath,delimiter):  
     
+    '''
+    Parameters:
+        fPath: The path in which the file is located. Can be a full/relative path
+        delimiter: The character that is seperating the data items
+    '''
+    
     
     nItems = 1
     originalData  = []
@@ -35,13 +41,15 @@ def read_file(fPath,delimiter):
    
 
 
-def main(fPath, delimiter, minSuppRate):
+
+def main(fPath, delimiter, minSupp):
     
     '''
     Parameters:
         fName: File name of the dataset
         delimiter: what is used to seperate the data in the file
         minSuppRate: minimum support rate of accepted frequencies
+        minSupp: minimum support frequency of accepted items
        
     '''
     
@@ -54,21 +62,31 @@ def main(fPath, delimiter, minSuppRate):
     ###  INITIALIZATION  ###
     ########################
     
-    # SINGLE ITEM FREQUENCY ARRAY [0,1,2,......,]
-    # EACH INDEX IS AN 'ITEM'
-    sif = np.zeros(nItems+1)    
+  
     
+    # I'LL UNCOMMENT THIS LATER
+    ################################
     # NUMBER OF TRANSACTIONS        
-    nTrans = len(DATASET)  
+    #nTrans = len(DATASET)  
     
     # MINIMUM SUPPORT ( RATE * # OF TRANSACTIONS)
-    minSupp = int(round(minSuppRate*nTrans,0))
+    #minSupp = int(round(minSuppRate*nTrans,0))
+    ################################
+    print('minSupp: ',minSupp)
+    
+    
+    
+    # SINGLE ITEM FREQUENCY ARRAY [0,1,2,......,]
+    # EACH INDEX IS AN 'ITEM'
+    sif = np.zeros(nItems+1)      
+    
     
 
     # ADD 1 TO THE ITEM IF IT'S IN THE TRANSACTION
     for trn in DATASET:
         sif[trn]+=1    
     
+    # LOGICAL ARRAY WHERE 1 IF ITEM'S FREQ. IS >= TO THE ACCEPTED FREQ. ELSE 0
     logicalIx = sif > minSupp
     
     # STORE THE 'ITEMS' THAT HAD A FREQUENCY > 'minSupp'
@@ -78,89 +96,230 @@ def main(fPath, delimiter, minSuppRate):
     # NUMBER OF 'fi'
     nfi = len(fi)
     
-    # WILL BE USED IN PLACE OF THE 'fi' FOR THE ALGORITHM
-    proxyIID = np.arange(1,nfi+1)
-
-    
-    print(nfi, '  ', fi)
     # ffi: FREQUENCY OF FREQUENT ITEMS
     ffi = sif[fi]
     
     # INDECES OF THE SORTED 'ffi' IN ASCENDING ORDER
-    idxSort = np.argsort(ffi)[::-1]
+    idxSort = np.argsort(ffi)  #(0: less freq )
     
-    # REMAINING ITEMSET ARRAY
-    ris = [ [] for ii in range(nTrans) ] 
+    # sfi: SORTED FREQ. ITEMS
+    # SORTED THE FREQ. ITEMS FROM SMALLEST FREQ. TO LARGEST FREQ.
+    sfi = fi[idxSort]
     
+    # WILL BE USED IN PLACE OF THE 'fi' FOR THE ALGORITHM
+    # INDEX 0 INDICATES THE SMALLEST FREQ. ITEM
+    proxyIID = np.arange(0,nfi)    
     
-    # AT THE MOMENT, THIS WILL ONLY WORK FOR 'fi' GREATER THAN 3
-    
-    # CREATE AN nCr(nfi,3) ARRAY 
-    sub3 = [[] for i in range(int(nCr(nfi,3)))]
-    
-    # CREATE AN nCr(nfi,2) ARRAY
-    sub2 = np.zeros(int(nCr(nfi,2)))
-    
-    
-    # CREATE A HASHKEY TO MAP ITEMSETS TO APPROPIATE SUBSETS
-    hashkey = np.zeros((nfi-1,2))
-    
-    for ii in range(nfi-1):
-        for kk in [1,2]:
-            
-            hashkey[ii,kk-1] = nCr(ii,kk)
-
-    print(hashkey)
-    ######################
-    ###  COMPUTATIONS  ###
-    ######################
-    
+    # WILL BE USED TO SELECT THE ITEMS PER TRANSACTION
     indicator = np.zeros(nItems+1,dtype=bool)
     
-    for trnIdx,trn in enumerate(DATASET):
-        
-        
+    # DATA STRUCTURE USED TO STORE THE FREQ. COUNT FOR 2 ITEMS AS WELL AS STORE TRANSACTION ID
+    # THE UPPER TRIANGLE WILL STORE THE FREQ COUNT
+    # THE LOWER TRIANGLE WILL STORE THE TRANSACTION ID
+    NL = np.zeros( (nfi,nfi ), dtype = int).tolist()
+    
+    # DATA STRUCTURE USED TO STORE THE L' FOR 2 ITEMS AS WELL AS L"
+    # THE UPPER TRIANGLE WILL STORE L'
+    # THE LOWER TRIANGLE WILL STORE L"  
+    LpLpp = np.zeros( (nfi,nfi ), dtype = int).tolist()
+    
+    
+
+    freq_marker = []
+    
+    # NOTE: IN THE LOOP, THE ALGO. WILL IGNORE THE ITEMSETS 
+    # THAT DO NOT HAVE THE FREQ. ITEMS. THE TID FOR THESE ITEMSETS WILL NOT BE RECORDED.
+    # IN THE PROCESS, THE ITEMSET THAT DO HAVE THE FREQ. ITEMS WILL BE REWRITTEN USING THE PROXY ID.
+    # THE PROXY ITEMSET WILL BE STORED IN THE TID.
+    for TID,itemset in enumerate(DATASET):
         
         # GET FREQUENT ITEMS FROM CURRENT ITEMSET
-        # FREQUENT ITEMS ARE SORTED BASED ON THE 'ffi'
-        indicator[trn] = 1
-        gfi = proxyIID[ indicator[ fi[idxSort] ] ]  # [1,2,....]  (1: most freq )
+        # FREQUENT ITEMS ARE SORTED BASED ON THE 'sfi'
+        indicator[itemset] = 1
+        logicalArray =  indicator[sfi]
+        curItemset = proxyIID[logicalArray]
         
-        
-        
-        
-        # GET THE SIZE OF THE 'gfi'
-        n_gfi = len(gfi)
-        
-        
-        if n_gfi > 3:
+        # STORE THE FREQ. COUNT FOR 2-ITEMSET (a,b)
+        if len(curItemset) == 2:
+            a,b = curItemset
+            NL[a][b] += 1
             
-            # STORE THE REMAINING ITEMSET AFTER TAKING OUT THE LAST THREE ELEMS
-            ris[trnIdx] = gfi[:-3]
+        # TRUNCATE THE CURRENT ITEMSET 
+        # STORE THE TID FOR LATER
+        elif len(curItemset) > 2:
             
-            # KEEP A REFERENCE TO THE LAST THREE ELEMS IN THE ITEMSET
-            ref = gfi[-3:]
+            a,b = curItemset[:2]
+            DATASET[TID] = curItemset[2:]
+                   
+            if isinstance(NL[b][a],int):
+                NL[b][a] = []
             
-            key = np.sum(hashkey[ :(ref[0]-2) , 1],dtype = int) + np.sum(hashkey[ :(ref[1]-2) , 0],dtype=int) + ref[2] - 1
-            
-            sub3[key].append(trnIdx)
+            NL[b][a].append(TID)
+        
+        # CLEAR THE INDICATOR FOR REUSE
+        indicator[itemset] = 0   
+        
+            	
 
-        
-        elif n_gfi == 2:
+    # GET A LIST OF LENGTHS FROM LIST OF LIST
+    funcLenLst = lambda x: list(map( len , x))
+    # GET THE SUM OF THE LENGTHS OF A LIST OF LIST
+    sumLen = lambda x: sum(funcLenLst(x))
+    
+    # a < b in terms of frequency
+    # a != b
+    for a in range(nfi):
+        for b in range(a+1,nfi):
             
-            ris[trnIdx] = None
+            Done = False
             
-            ref = gfi[-2:]
-            key = np.sum(hashkey[ :(ref[0]-2) , 0],dtype=int) + ref[1] - 1
+            while(not(Done)):
+                
+                # DEFINE THE LIST IF NOT PRESENT
+                if isinstance(NL[b][a],int):
+                    NL[b][a] = []
+                if isinstance(LpLpp[a][b],int):
+                    LpLpp[a][b] = []
+                if isinstance(LpLpp[b][a],int):
+                    LpLpp[b][a] = []
+                
+                
+                print('{}: {}  {}  {}  {}'.format((a,b),NL[a][b], len( NL[b][a] ), len(LpLpp[a][b]), len(LpLpp[b][a])))
+                print('{}: {}  {}  {}  {}'.format( (a,b),NL[a][b], len( NL[b][a] ), sumLen( LpLpp[a][a+1:b+1] ), sumLen( [x[a] for x in LpLpp[a+1:b+1]] ) ) )
+                print('')
+                
+                # N_a_b + |L_a_b| + |L'_a_b| + |L"_a_b|
+                if NL[a][b] + len( NL[b][a] ) + len(LpLpp[a][b]) + len(LpLpp[b][a]) >= minSupp:
                     
+                    Done = True
+                    freq_marker.append([a,b])
+                    
+                # N_a_b       + |L_a_b|         + (  |L'_a_{a+1}| + ... + |L'_a_{b-1}| + |L'_a_b|) + ( |L''_a_{a+1}|+ ... + |L"_a_{b-1}| + |L''_a_b| )
+                elif NL[a][b] + len( NL[b][a] ) + sumLen(LpLpp[a][a+1:b+1])  +  sumLen([x[a] for x in LpLpp[a+1:b+1]])  <  minSupp:
+                    
+                    
+                    Done = True
+                
+                # THIS SECTION IS STILL A WORK IN PROGRESS
+                else:
+                    print('other')
+                    
+                    # FIND c'
+                    cp = a + 1 + np.argmax( funcLenLst( LpLpp[a][a+1:b+1] ) )
+                    
+                    # Find c"
+                    cpp = a + 1 + np.argmax( funcLenLst( [ x[a] for x in LpLpp[a+1:b+1] ] ) )
+                    
+                    # GET L_a_c'
+                    lp_ac = LpLpp[a][cp]
+                    
+                    # GET L_a_c"
+                    lpp_ac = LpLpp[cpp][a]
+                    
+                    # |L_a_c'| < |L_a_c"|
+                    if len(lp_ac) < len(lpp_ac):
+                        
+                        for pointer,tid  in lpp_ac:
+                            
+                            curItem = DATASET[tid]
+                            
+                            
+                            if isinstance(LpLpp[curItem[pointer]][b],int):
+                                LpLpp[curItem[pointer]][b] = []
+                            
+
+                            LpLpp[curItem[pointer]][b].append([pointer+1,tid])  
+                            
+                        # zero out after redistribution ?
+                        LpLpp[cpp][a] = []  
+                        
+                    else:
+                        
+                        
+                        for tid  in lp_ac:
+                        
+                            curItem = DATASET[tid]
+                            
+                            if isinstance(LpLpp[b][cp],int):
+                                LpLpp[b][cp] = []
+                            
+                            LpLpp[b][cp].append(tid)   
+
+                        # zero out after redistribution ?
+                        LpLpp[a][cp] = []
+                                          
+                                                   
+		
+
+            # redistribute
+            idx_for_del = []
+            for idx,tid in enumerate(NL[b][a]):
+                
+                curItem = DATASET[tid]
+                c = curItem[0]
+                
+                # ITEMSET ONLY HAS ONE ITEM LEFT STORED
+                # REDISTRIBUTE THIS ITEMSET TO THE FREQ. COUNT
+                if len(curItem) == 1:
+                    
+                    NL[a][b] +=1
+                    NL[a][c] +=1
+                    NL[b][c] +=1
+                    
+                    # RECORD WHAT INDEX WILL BE DELETED
+                    idx_for_del.append(idx)
+                else:
+                    
+                    # DEFINE IN THE LIST IF NOT PRESENT
+                    if isinstance(NL[c][b] , int ):
+                        NL[c][b] = [] # L_b_c
+                    if isinstance ( LpLpp[a][c] , int):
+                        LpLpp[a][c] = [] # L'_a_c
+                    
+                    
+                    # STORE TID IN L_#_b
+                    NL[c][b].append(tid)
+                    
+                    # STORE L'_a_#
+                    LpLpp[a][c].append(tid)
+                    
+                    # POP THE FIRST ELEMENT OFF. NO LONGER IN USE.
+                    DATASET[tid] = DATASET[tid][1:]
+                    
+            # L_a_b DELETE TID THAT NO LONGER RELEVENT 
+            NL[b][a] = np.delete(NL[b][a], idx_for_del)     
+
+
+    print('')
+    print('Freq 2-Itemsets')
+    for a,b in freq_marker:
         
-            sub2[key] +=1
-        
-        indicator[trn] = 0
-    
-    for idx,trn in enumerate(sub2):
-        print(idx,' ',trn)  
-    
+        #print( (sfi[a],sfi[b]), ':',NL[a][b])
+        #print( (a,b), ':',NL[a][b])
+        print('Proxy:({},{}) Actual:({},{})'.format(a,b,sfi[a],sfi[b]))
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -172,11 +331,11 @@ def main(fPath, delimiter, minSuppRate):
 if __name__ == '__main__':
     
     directory = './data/'
-    filename = 'nTrn50_nItems5.csv'
+    filename = 'nTrn50_nItems7.csv'
     file_path = os.path.join(directory, filename)
     
     
     delimiter="," 
-    minSuppRate= 0.2
+    minSuppRate= 8
     
     main(file_path,delimiter,minSuppRate)
