f7963607e0b90aca66187d51cca4cd6561a5673f
Luis Vargas Tamayo <fatherluis98@gmail.com>
2020-09-28 23:56:36 -0500

    Working Code
    *Redesigned datastructure to fit for higher levels later. Commented .

--- freq_mining.py
+++ freq_mining.py
@@ -5,6 +5,7 @@
 import os.path
 import numpy as np
 from scipy.special import comb as nCr
+import time
 
 
 
@@ -42,7 +43,7 @@ def read_file(fPath,delimiter):
 
 
 
-def main(fPath, delimiter, minSupp):
+def main(fPath, delimiter, minSuppRate):
     
     '''
     Parameters:
@@ -52,11 +53,13 @@ def main(fPath, delimiter, minSupp):
         minSupp: minimum support frequency of accepted items
        
     '''
-    
+    t1=time.perf_counter()
     # STORES A LIST OF 'SETS'
     # EACH ELEMENT IS A TRANSACTION WITH SOME 'LIST'
     # nItems: number of unique elements in the dataset
     DATASET,nItems = read_file(fPath,delimiter)
+    
+    t2=time.perf_counter()
     
     ########################
     ###  INITIALIZATION  ###
@@ -67,10 +70,10 @@ def main(fPath, delimiter, minSupp):
     # I'LL UNCOMMENT THIS LATER
     ################################
     # NUMBER OF TRANSACTIONS        
-    #nTrans = len(DATASET)  
+    nTrans = len(DATASET)  
     
     # MINIMUM SUPPORT ( RATE * # OF TRANSACTIONS)
-    #minSupp = int(round(minSuppRate*nTrans,0))
+    minSupp = int(round(minSuppRate*nTrans,0))
     ################################
     print('minSupp: ',minSupp)
     
@@ -113,19 +116,17 @@ def main(fPath, delimiter, minSupp):
     # WILL BE USED TO SELECT THE ITEMS PER TRANSACTION
     indicator = np.zeros(nItems+1,dtype=bool)
     
-    # DATA STRUCTURE USED TO STORE THE FREQ. COUNT FOR 2 ITEMS AS WELL AS STORE TRANSACTION ID
-    # THE UPPER TRIANGLE WILL STORE THE FREQ COUNT
-    # THE LOWER TRIANGLE WILL STORE THE TRANSACTION ID
-    NL = np.zeros( (nfi,nfi ), dtype = int).tolist()
-    
-    # DATA STRUCTURE USED TO STORE THE L' FOR 2 ITEMS AS WELL AS L"
-    # THE UPPER TRIANGLE WILL STORE L'
-    # THE LOWER TRIANGLE WILL STORE L"  
-    LpLpp = np.zeros( (nfi,nfi ), dtype = int).tolist()
+    print('sff:{}'.format(ffi[idxSort]))
+    print('sfi:{}'.format(sfi))
+    print('pro:{}'.format(proxyIID))
     
     
-
-    freq_marker = []
+    
+    generate_lst = lambda N: [ [0,[],[],[]] for i in range(N) ]
+    
+    struc2D = [generate_lst(n) for n in range(nfi,0,-1)]
+    
+    
     
     # NOTE: IN THE LOOP, THE ALGO. WILL IGNORE THE ITEMSETS 
     # THAT DO NOT HAVE THE FREQ. ITEMS. THE TID FOR THESE ITEMSETS WILL NOT BE RECORDED.
@@ -142,118 +143,142 @@ def main(fPath, delimiter, minSupp):
         # STORE THE FREQ. COUNT FOR 2-ITEMSET (a,b)
         if len(curItemset) == 2:
             a,b = curItemset
-            NL[a][b] += 1
+            
+            # ADD 1 TO THE FREQ. OF (a,b)-ITEMSET
+            struc2D[a][b-(a+1)][0] += 1
+            
+
             
         # TRUNCATE THE CURRENT ITEMSET 
         # STORE THE TID FOR LATER
         elif len(curItemset) > 2:
             
+            # GET THE FIRST TWO ELEM FOR INDEXING
             a,b = curItemset[:2]
+            
+            # TRUNCATE THE FIRST TWO ITEMS
             DATASET[TID] = curItemset[2:]
                    
-            if isinstance(NL[b][a],int):
-                NL[b][a] = []
-            
-            NL[b][a].append(TID)
+            # INDEX THE TID INTO THE DATA STRUCTURE
+            # RECALL THAT B MUST BE SHIFTED BY (a+1)
+            struc2D[a][b-(a+1)][1].append(TID)
         
         # CLEAR THE INDICATOR FOR REUSE
-        indicator[itemset] = 0   
-        
-            	
-
-    # GET A LIST OF LENGTHS FROM LIST OF LIST
-    funcLenLst = lambda x: list(map( len , x))
-    # GET THE SUM OF THE LENGTHS OF A LIST OF LIST
-    sumLen = lambda x: sum(funcLenLst(x))
+        indicator[itemset] = 0       
+    
+    
+    # KEEP TRACK OF FREQ. 2-ITEMSETS
+    # WILL STORE ELEM AS SUCH: [a,b,#]
+    # WHERE a,b ARE THE PROXY ELEMS AND # IS THE FREQ.
+    freq_marker = []
+    
+    
+    
+  
     
     # a < b in terms of frequency
     # a != b
     for a in range(nfi):
-        for b in range(a+1,nfi):
-            
+        for b in range(nfi - (a+1)): # PROXY VALUE SHIFT BY (a+1) 
+        
+            # STATE IF FREQ. OF (a,b)-ITEMSET HAS BEEN DETERMINED
             Done = False
             
             while(not(Done)):
                 
-                # DEFINE THE LIST IF NOT PRESENT
-                if isinstance(NL[b][a],int):
-                    NL[b][a] = []
-                if isinstance(LpLpp[a][b],int):
-                    LpLpp[a][b] = []
-                if isinstance(LpLpp[b][a],int):
-                    LpLpp[b][a] = []
                 
-                
-                print('{}: {}  {}  {}  {}'.format((a,b),NL[a][b], len( NL[b][a] ), len(LpLpp[a][b]), len(LpLpp[b][a])))
-                print('{}: {}  {}  {}  {}'.format( (a,b),NL[a][b], len( NL[b][a] ), sumLen( LpLpp[a][a+1:b+1] ), sumLen( [x[a] for x in LpLpp[a+1:b+1]] ) ) )
-                print('')
+                #print("\n({},{}): N:{}  L:{}  Lp:{}  Lpp:{}".format(a,b,struc2D[a][b][0],len(struc2D[a][b][1]),len(struc2D[a][b][2]),len(struc2D[a][b][3])))
                 
                 # N_a_b + |L_a_b| + |L'_a_b| + |L"_a_b|
-                if NL[a][b] + len( NL[b][a] ) + len(LpLpp[a][b]) + len(LpLpp[b][a]) >= minSupp:
-                    
-                    Done = True
-                    freq_marker.append([a,b])
-                    
-                # N_a_b       + |L_a_b|         + (  |L'_a_{a+1}| + ... + |L'_a_{b-1}| + |L'_a_b|) + ( |L''_a_{a+1}|+ ... + |L"_a_{b-1}| + |L''_a_b| )
-                elif NL[a][b] + len( NL[b][a] ) + sumLen(LpLpp[a][a+1:b+1])  +  sumLen([x[a] for x in LpLpp[a+1:b+1]])  <  minSupp:
-                    
-                    
-                    Done = True
+                minThreshold = struc2D[a][b][0] + len(struc2D[a][b][1]) + len(struc2D[a][b][2]) + len(struc2D[a][b][3])
                 
-                # THIS SECTION IS STILL A WORK IN PROGRESS
+                ################
+                ## CONDITION 1
+                ################
+                
+                # CHECK IF FREQ. IS GREATER THAN THE MIN SUPPORT
+                if minThreshold >= minSupp:
+
+                    Done = True
+                    # MARK (a,b)-ITEMSET AS FREQ.
+                    freq_marker.append([a,b,minThreshold])
+                    continue 
+                
+                ################
+                ## CONDITION 2
+                ################                
+                
+                #print("({},{}): N:{}  L:{}  Lp:{}  Lpp:{}".format(a,b,struc2D[a][b][0],len(struc2D[a][b][1]),sum([len(struc2D[a][i][2]) for i in range(b+1)]),sum([len(struc2D[a][i][3]) for i in range(b+1)])))
+
+                # N_a_b  + |L_a_b| + (  |L'_a_0| + ... + |L'_a_b|) + ( |L''_a_0|+ ...  + |L''_a_b| )
+                predThreshold = struc2D[a][b][0] + len(struc2D[a][b][1]) + sum([len(struc2D[a][i][2]) for i in range(b+1)]) + sum([len(struc2D[a][i][3]) for i in range(b+1)])
+        
+                # CHECK IF FREQ. IS LESS THAN THE MIN SUPPORT
+                # IF SO, THEN FREQ. FOR THIS (a,b)-ITEMSET WONT MEET THE REQ.
+                if predThreshold < minSupp:
+                    Done = True
+                    continue
+
+
+                ################
+                ## CONDITION 3
+                ################
+
+                # FIND c' and c" SUCH WHERE max(|L_a_c|) , WHERE max(|L"_a_c|) 
+                cp = np.argmax(  [len(struc2D[a][i][2]) for i in range(b)]  )
+                cpp = np.argmax( [len(struc2D[a][i][3]) for i in range(b)]  )
+    
+
+                # | L'_ac| < |L"_ac|
+                if len(struc2D[a][cp][2]) < len(struc2D[a][cpp][3]):
+
+                    # REDISTRIBUTE L"_ac" to L"_bc
+    
+
+                    # ITERATE THROUGH THE ELEMS STORED IN L"_ac"
+                    for pointer,TID in struc2D[a][cpp][3]:
+                        
+                        curItem = DATASET[TID]
+                        
+                        # MAKE SURE THAT THE ITEMSET HAS ANOTHER ELEM TO POINT
+                        if len(curItem) > pointer+1 :
+                            
+                            
+                            proxB = curItem[pointer]
+                            proxC = curItem[pointer+1] - (proxB+1)
+                                                    
+
+                            struc2D[proxB][proxC][3].append( [pointer+1 , TID] ) 
+                            
+                    # EMPTY OUT L"_ac"
+                    struc2D[a][cpp][3] = []
+    
+      
                 else:
-                    print('other')
                     
-                    # FIND c'
-                    cp = a + 1 + np.argmax( funcLenLst( LpLpp[a][a+1:b+1] ) )
+                    # REDISTRIBUTE L'_ac' to L"_bc
                     
-                    # Find c"
-                    cpp = a + 1 + np.argmax( funcLenLst( [ x[a] for x in LpLpp[a+1:b+1] ] ) )
-                    
-                    # GET L_a_c'
-                    lp_ac = LpLpp[a][cp]
-                    
-                    # GET L_a_c"
-                    lpp_ac = LpLpp[cpp][a]
-                    
-                    # |L_a_c'| < |L_a_c"|
-                    if len(lp_ac) < len(lpp_ac):
+                    # ITERATE THROUGH THE ELSM STORED IN L'_ac'
+                    for TID in struc2D[a][cp][2]:
+    
+                        curItem = DATASET[TID]   
                         
-                        for pointer,tid  in lpp_ac:
-                            
-                            curItem = DATASET[tid]
-                            
-                            
-                            if isinstance(LpLpp[curItem[pointer]][b],int):
-                                LpLpp[curItem[pointer]][b] = []
-                            
+                        c = curItem[0]      
+                        proxB = cp +(a+1) # SHIFT BY (a+1) 
+                        proxC = c - (proxB+1) # SHIFT BY (ProxB + 1)
 
-                            LpLpp[curItem[pointer]][b].append([pointer+1,tid])  
-                            
-                        # zero out after redistribution ?
-                        LpLpp[cpp][a] = []  
                         
-                    else:
-                        
-                        
-                        for tid  in lp_ac:
-                        
-                            curItem = DATASET[tid]
-                            
-                            if isinstance(LpLpp[b][cp],int):
-                                LpLpp[b][cp] = []
-                            
-                            LpLpp[b][cp].append(tid)   
-
-                        # zero out after redistribution ?
-                        LpLpp[a][cp] = []
-                                          
-                                                   
-		
-
-            # redistribute
-            idx_for_del = []
-            for idx,tid in enumerate(NL[b][a]):
+                        struc2D[proxB][proxC][3].append([1,TID])
+                      
+                    # EMPTY OUT L'a_c'
+                    struc2D[a][cp][2] = []
+             
+                    
+             
+            
+            # REDISTRIBUTE L_ab to L'ac and also to L_bc
+            # ITERATE THROUGH THE ELEMS STORED IN L_ab
+            for idx,tid in enumerate(struc2D[a][b][1]):
                 
                 curItem = DATASET[tid]
                 c = curItem[0]
@@ -262,63 +287,60 @@ def main(fPath, delimiter, minSupp):
                 # REDISTRIBUTE THIS ITEMSET TO THE FREQ. COUNT
                 if len(curItem) == 1:
                     
-                    NL[a][b] +=1
-                    NL[a][c] +=1
-                    NL[b][c] +=1
+                    proxB = b + (a+1)
+                    proxC = c - (proxB+1)
                     
-                    # RECORD WHAT INDEX WILL BE DELETED
-                    idx_for_del.append(idx)
+                    
+                    struc2D[a][c-(a+1)][0] += 1
+                    struc2D[proxB][proxC][0] += 1
+                    
+                    
                 else:
                     
-                    # DEFINE IN THE LIST IF NOT PRESENT
-                    if isinstance(NL[c][b] , int ):
-                        NL[c][b] = [] # L_b_c
-                    if isinstance ( LpLpp[a][c] , int):
-                        LpLpp[a][c] = [] # L'_a_c
-                    
+                    proxB = b + (a+1)
+                    proxC = c - (proxB+1)
                     
                     # STORE TID IN L_#_b
-                    NL[c][b].append(tid)
+                    struc2D[proxB][proxC][1].append(tid)
+ 
                     
                     # STORE L'_a_#
-                    LpLpp[a][c].append(tid)
+                    struc2D[a][c-(a+1)][2].append(tid)
+
                     
                     # POP THE FIRST ELEMENT OFF. NO LONGER IN USE.
                     DATASET[tid] = DATASET[tid][1:]
-                    
-            # L_a_b DELETE TID THAT NO LONGER RELEVENT 
-            NL[b][a] = np.delete(NL[b][a], idx_for_del)     
+                
+                
+            # EMPTY OUT L_ab
+            struc2D[a][b][1] = []
 
+        # CLEAR ALL OF N_a# , L_a# , L'_a#, L"_a#
+        struc2D[a] = 0 
+
+
+
+
+    t3=time.perf_counter()
+    
+    print('\nData reading time:',t2-t1) 
+    print('Mining time:      ',t3-t2)
+    print('Total time:       ',t3-t1)
 
     print('')
     print('Freq 2-Itemsets')
-    for a,b in freq_marker:
+    print('-----------------------------')
+    for a,b,freq in freq_marker:
         
         #print( (sfi[a],sfi[b]), ':',NL[a][b])
         #print( (a,b), ':',NL[a][b])
-        print('Proxy:({},{}) Actual:({},{})'.format(a,b,sfi[a],sfi[b]))
+        print('Proxy:({},{}) Actual:({},{}) minFreq: {}'.format(a,b+(a+1),sfi[a],sfi[b+(a+1)],freq))
             
+    print('-----------------------------')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
@@ -331,11 +353,36 @@ def main(fPath, delimiter, minSupp):
 if __name__ == '__main__':
     
     directory = './data/'
-    filename = 'nTrn50_nItems7.csv'
+    
+    ### MY DATASET
+    # filename = 'nTrn50_nItems7.csv'
+    # file_path = os.path.join(directory, filename)
+    # delimiter="," 
+    # minSuppRate= 0.15
+    
+    ### KOSARAK DATASET
+    filename = 'kosarak.dat'
     file_path = os.path.join(directory, filename)
+    delimiter=" "   
+    minSuppRate = 0.025
     
     
-    delimiter="," 
-    minSuppRate= 8
+    
+    
+    
     
     main(file_path,delimiter,minSuppRate)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
